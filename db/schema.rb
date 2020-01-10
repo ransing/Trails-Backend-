f814c897dddcfc71e665c6626fb15d9b5dc306fc
viraj <virajransing@gmail.com>
2020-01-10 17:34:18 -0500

    removed type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_205845) do
+ActiveRecord::Schema.define(version: 2020_01_10_223259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_205845) do
     t.string "conditionDate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "type"
     t.string "category"
   end
 
